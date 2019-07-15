addbc15b9d60de59632b5cd344a116608630a8c8
Ben Fradet <benjamin.fradet@gmail.com>
2019-07-15 16:01:01 +0200

    Bump Scala to 2.12 (closes #9)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
 
   object V {
     // Scala
-    val igluClient       = "0.6.0-M6"
+    val igluClient       = "0.6.0-M9"
     val circe            = "0.11.1"
     val schemaDdl        = "0.10.0-M3"
     val scalaCheck       = "1.14.0"
